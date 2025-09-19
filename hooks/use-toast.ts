49f15016eb9cc06fd0584d9ce17de9b13f9e65e0
Mohammed Kamil <168296043+CodeMaesttro@users.noreply.github.com>
2025-09-19 18:05:47 -0400

    Update use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -8,6 +8,16 @@ import type {
   ToastProps,
 } from "@/components/ui/toast"
 
+const TOAST_LIMIT = 1
+const TOAST_REMOVE_DELAY = 1000000
+
+type ToasterToast = ToastProps & {
+  id: string
+  title?: React.ReactNode
+  description?: React.ReactNode
+  action?: ToastActionElement
+}
+
 
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
